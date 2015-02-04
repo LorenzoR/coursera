aa2c19db3f9d6b93a33eba2eaa33aef89fc5af4a
LorenzoR <lorenzorodrigo2000@hotmail.com>
2015-02-04 12:49:39 -0500

    merge ex2

--- machineLearning/ex1/ex1_multi.m
+++ machineLearning/ex1/ex1_multi.m
@@ -105,7 +105,7 @@ fprintf('\n');
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 price = 0; % You should change this
-
+price = [1, 1650, 3] *theta;
 
 % ============================================================
 
